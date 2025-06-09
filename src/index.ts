1f767a49cef262bb1c352dcd781079d570b14cff
walid <walidelkhemiri@gmail.com>
2025-06-09 07:54:56 +0100

    First commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+console.log('hello world');
+/**
+   Run code: 
+   1) npx tsc
+      node dist/index.js
+   or
+   2) npx ts-node src/index.ts
+
+   Format all files in src directory: npx prettier --write src
+
+   Lint the code: npx eslint .
+ */
